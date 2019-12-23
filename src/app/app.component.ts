9217c8f518f71d9da987fc8b3cd69cfce97cc7b8
Rutuja <rutuja.rds21@gmail.com>
2019-12-23 17:58:51 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  isCollapsed = false;
+  repetativeDiv = [{ divOne: 'div1'},{ divTwo: 'div2'}]
+}
